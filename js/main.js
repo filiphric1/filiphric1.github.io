2deb0e5d6bfc0e25462a5ed33680c4968f1e0a36
filipespes1 <filip.hric@seznam.cz>
2020-01-08 22:17:20 +0100

    232

--- js/main.js
+++ js/main.js
@@ -15,14 +15,7 @@ $(function() {
         }
     });
 });
-// Vyznačení sekce po kliknutí na link v menu
-$(function() {
-    $('.main-menu a').click(function() {
-        $('.main-menu a').removeClass(
-            'active');
-        $(this).addClass('active');
-    });
-});
+
 // Při kliknutí na logo odznačení aktivního linku v menu
 $(function() {
     $('.logo').click(function() {
@@ -50,4 +43,80 @@ $(function() {
         $('.main-menu, .socials').toggleClass(
             'mobile');
     });
+});
+// Označení active linku v menu při kliknutí/scrollování na sekci
+// cache the navigation links
+var $navigationLinks = $('.main-menu > li > a');
+// cache (in reversed order) the sections
+var $sections = $($(".scrollSection").get().reverse());
+
+// map each section id to their corresponding navigation link
+var sectionIdTonavigationLink = {};
+$sections.each(function() {
+    var id = $(this).attr('id');
+    sectionIdTonavigationLink[id] = $('.main-menu > li > a[href=\\#' + id + ']');
+});
+
+// throttle function, enforces a minimum time interval
+function throttle(fn, interval) {
+    var lastCall, timeoutId;
+    return function() {
+        var now = new Date().getTime();
+        if (lastCall && now < (lastCall + interval)) {
+            // if we are inside the interval we wait
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(function() {
+                lastCall = now;
+                fn.call();
+            }, interval - (now - lastCall));
+        } else {
+            // otherwise, we directly call the function
+            lastCall = now;
+            fn.call();
+        }
+    };
+}
+
+function highlightNavigation() {
+    // get the current vertical position of the scroll bar
+    var scrollPosition = $(window).scrollTop();
+
+    // iterate the sections
+    $sections.each(function() {
+        var currentSection = $(this);
+        // get the position of the section
+        var sectionTop = currentSection.offset().top - 100;
+
+        // if the user has scrolled over the top of the section
+        if (scrollPosition >= sectionTop) {
+            // get the section id
+            var id = currentSection.attr('id');
+            // get the corresponding navigation link
+            var $navigationLink = sectionIdTonavigationLink[id];
+            // if the link is not active
+            if (!$navigationLink.hasClass('active')) {
+                // remove .active class from all the links
+                $navigationLinks.removeClass('active');
+                // add .active class to the current link
+                $navigationLink.addClass('active');
+            }
+
+            // we have found our section, so we return false to exit the each loop
+            return false;
+        }
+    });
+}
+
+$(window).scroll(throttle(highlightNavigation, 500));
+
+// Fix pro označení kontaktu, i když scrolltop není v jeho sekci
+$(window).scroll(function() {
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $navigationLinks.removeClass('active');
+        $('.scrollFix').addClass(
+            'active');
+    } else {
+        $('.scrollFix').removeClass(
+            'active');
+    }
 });
