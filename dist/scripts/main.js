a4d3fe718dead20a10b79b75cdcb81bd81094daa
Filip Hric <filip.hric@seznam.cz>
2021-08-26 22:24:26 +0200

    Navbar template & styles added

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
     // vlastní skripty
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".custom-navbar-menu");
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
 
 });
